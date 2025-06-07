d69a9a4c2dad5360655c49ed430d4f549046ed90
zanybarlee <zanybarlee@yahoo.com.sg>
2025-06-08 01:23:20 +0800

    Akira

---
+++ fetchfw.sh
@@ -0,0 +1,6 @@
+git checkout main
+git fetch origin
+git merge origin/main
+git add .
+git commit -m "Your update message"
+git push upstream main
---
+++ starttrom.sh
@@ -0,0 +1,3 @@
+rm packages/server/.env
+cp packages/server/.env.tromai.example packages/server/.env
+pnpm start
